37e8b06e47f552ef102c14b5396e249de8082ddd
tchatz <tchatz@icloud.com>
2016-06-28 01:51:06 +0300

    add status code 3 (token expired)

--- home/home.controller.js
+++ home/home.controller.js
@@ -5,18 +5,26 @@
             .module('app')
             .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', '$http', '$location', 'ApiService', '$rootScope'];
-    function HomeController($scope, $http, $location, ApiService, $rootScope) {
-         $scope.$location = $location;
+    HomeController.$inject = ['$scope', '$http', '$location', 'ApiService', '$rootScope', '$cookieStore'];
+    function HomeController($scope, $http, $location, ApiService, $rootScope, $cookieStore) {
+        $scope.$location = $location;
         $scope.SelectedUser = '';
 
 
         $scope.loadMovies = function () {
             ApiService.GetAllMovies()
                     .then(function (movies) {
-                        $scope.UserMovies = false;
-                        $scope.movies = movies.data;
-                        console.log($scope.movies);
+                        if (movies.status.code == '3') {
+                            alert("s");
+                            $http.defaults.headers.common['deftoken'] = movies.status.newToken;
+                            $rootScope.globals.currentUser.token = movies.status.newToken;
+                            $cookieStore.put('globals', $rootScope.globals);
+                            $scope.loadMovies();
+                        } else {
+                            $scope.UserMovies = false;
+                            $scope.movies = movies.data;
+                            console.log($scope.movies);
+                        }
                     });
         }
 
@@ -24,10 +32,17 @@
         $scope.loadMoviesById = function (id) {
             ApiService.GetAllMoviesById(id)
                     .then(function (movies) {
-                        $scope.UserMovies = true;
-                        $scope.SelectedUser = id;
-                        $scope.movies = movies.data;
-                        console.log($scope.movies);
+                        if (movies.status.code == '3') {
+                            $http.defaults.headers.common['deftoken'] = movies.status.newToken;
+                            $rootScope.globals.currentUser.token = movies.status.newToken;
+                            $cookieStore.put('globals', $rootScope.globals);
+                            $scope.loadMoviesById(id);
+                        } else {
+                            $scope.UserMovies = true;
+                            $scope.SelectedUser = id;
+                            $scope.movies = movies.data;
+                            console.log($scope.movies);
+                        }
                     });
         }
 
@@ -35,13 +50,20 @@
         $scope.loadMovies();
 
         $scope.giveVote = function (vote, movieId) {
-            $http.post($rootScope.app.apiUrl+'voteMovie', {vote: vote, movie: movieId})
+            $http.post($rootScope.app.apiUrl + 'voteMovie', {vote: vote, movie: movieId})
                     .success(function (response) {
-                        console.log(response);
-                        if ($scope.UserMovies) {
-                            $scope.loadMoviesById($scope.SelectedUser);
+                        if (response.status.code == '3') {
+                            $http.defaults.headers.common['deftoken'] = response.status.newToken;
+                            $rootScope.globals.currentUser.token = movies.status.newToken;
+                            $cookieStore.put('globals', $rootScope.globals);
+                            $scope.giveVote(vote, movieId);
                         } else {
-                            $scope.loadMovies();
+                            console.log(response);
+                            if ($scope.UserMovies) {
+                                $scope.loadMoviesById($scope.SelectedUser);
+                            } else {
+                                $scope.loadMovies();
+                            }
                         }
                     });
         }
